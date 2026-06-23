32fd40eafdfdc011be330d8eb7db47bd60d0c3e6
2233077633-oss <2233077633@correo.ler.uam.mx>
2026-06-23 14:58:37 -0600

    ultimo

--- sketch.js
+++ sketch.js
@@ -5,10 +5,13 @@ let pantalon = 0;
 let ropainterior = 0;
 let zapatos = 0;
 
-let buttonColor = 250;
-let buttonColoru = 250;
-let buttonColora = 250;
-let buttonColore = 250;
+let buttonColorc = 250;
+let buttonColorp = 250;
+let buttonColorpp = 250;
+let buttonColorcc = 250;
+let buttonColorz = 250;
+
+
 
 let img1;
 let img2;
@@ -43,37 +46,60 @@ function draw()
     text("¿Cuántas prendas traes el día de hoy?" ,200, 400);
 
     noStroke();
-    fill(buttonColor);
-    square(200,500,150);
-    fill(buttonColoru);
-    square(400,500,150); //botón
-    fill(buttonColora);
-    square(600,500,150);
-    fill(buttonColore);
-    square(800,500,150);
+    fill(buttonColorc);
+    square(250,500,150);
+    fill(buttonColorp);
+    square(480,500,150); //botón
+    fill(buttonColorpp);
+    square(700,500,150);
+    fill(buttonColorcc);
+    square(950,500,150);
+    fill(buttonColorz);
+    square(1200,500,150);
+    fill(0);
+    square(250,700,150);
+    fill(0);
+    square(480,700,150);
+    fill(0);
+    square(700,700,150);
+    fill(0);
+    square(950,700,150);
+    fill(0);
+    square(1200,700,150);
+    fill(0);
 
 
-    image(img1,230,520,100,100);
-    image(img2,420,520,100,100);
-    image(img3,600,520,100,100);
-    image(img4,800,520,100,100);
-    image(img5,1100,520,100,100);
+
+
+
+    image(img1,275,520,100,100);
+    image(img2,505,520,100,100);
+    image(img3,725,520,100,100);
+    image(img4,970,520,100,100);
+    image(img5,1220,520,100,100);
 
 }
 
 function mousePressed() {
-    if (mouseX >= 200 && mouseX <= 350 && mouseY >= 500 && mouseY <= 650) {
-        buttonColor = buttonColor === 255 ? color(0, 200, 0) : 255;
+    if (mouseX >= 250 && mouseX <= 400 && mouseY >= 500 && mouseY <= 650) {
+        buttonColorc = buttonColorc === 255 ? color(0, 255, 0) : 255;
     }
 
-    if(mouseX >= 400 && mouseX <= 550 && mouseY >= 500 && mouseY <= 650) {
-        buttonColoru = buttonColoru === 255 ? color(0, 200, 0) : 255;
+    if(mouseX >= 450 && mouseX <= 625 && mouseY >= 500 && mouseY <= 650) {
+        buttonColorp = buttonColorp === 255 ? color(0, 200, 0) : 255;
         
     }
-    if(mouseX >= 600 && mouseX <= 750 && mouseY >= 500 && mouseY <= 650) {
-        buttonColora = buttonColora === 255 ? color(0, 200, 0) : 255;
+    if(mouseX >= 700 && mouseX <= 850 && mouseY >= 500 && mouseY <= 650) {
+        buttonColorpp = buttonColorpp === 255 ? color(0, 200, 0) : 255;
     }
-    if(mouseX >= 800 && mouseX <= 950 && mouseY >= 500 && mouseY <= 650) {
-        buttonColore = buttonColore === 255 ? color(0, 200, 0) : 255;
+    if(mouseX >= 950 && mouseX <= 1100 && mouseY >= 500 && mouseY <= 650) {
+        buttonColorcc = buttonColorcc === 255 ? color(0, 200, 0) : 255;
+    }
+    if(mouseX >= 1200 && mouseX <= 1350 && mouseY >= 500 && mouseY <= 650) {
+        buttonColorz = buttonColorz === 255 ? color(0, 200, 0) : 255;
+    }
+
+    if (buttonColorc && buttonColorp && buttonColorpp && buttonColorcc && buttonColorz) {
+        console.log("4 Litros de agua");
     }
 }
