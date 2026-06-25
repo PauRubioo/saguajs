72a1c761445409276bd4043c6065298a932ce8e3
2233077633-oss <2233077633@correo.ler.uam.mx>
2026-06-24 22:35:10 -0600

    diez

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
-
 var ch = false;
 var pl = false;
 var pa = false;
 var ca = false;
 var za = false;
-
+var cin = false;
+var acc = false;
+var go = false;
+var le = false;
+var mo = false;
 
 
 let litros = 0;
@@ -14,10 +17,15 @@ let img2;
 let img3;
 let img4;
 let img5;
+let img6;
+let img7;
+let img8;
+let img9;
+let img10;
 
 function setup() 
 {
-    createCanvas(1800,1000);
+    createCanvas(1800,1200);
     background(220);
     
 }
@@ -26,7 +34,7 @@ function setup()
 
 function draw()
 {
-    background(240);
+    background(46, 218, 236);
     textSize(80);
     fill(0);
     textFont("Times New Roman"); 
@@ -35,7 +43,7 @@ function draw()
     text("¿Cuántas prendas traes el día de hoy?" ,200, 400);
     
     if (ch){
-        fill(39,245,49);    
+        fill(46, 218, 236);    
     }else{
         fill(255);
     }
@@ -43,7 +51,7 @@ function draw()
     square(250,500,150);
 
     if (pl){
-        fill(39,245,49);
+        fill(46, 218, 236);
     }else{
         fill(255);
     }
@@ -51,7 +59,7 @@ function draw()
     square(480,500,150);
     
     if (pa){
-        fill(39,245,49);
+        fill(46, 218, 236);
     }else{
         fill(255);
     }
@@ -59,7 +67,7 @@ function draw()
     square(700,500,150);
 
     if (ca){
-        fill(39,245,49);
+        fill(46, 218, 236);
     }else{
         fill(255);
     }
@@ -67,26 +75,70 @@ function draw()
     square(950,500,150);
 
     if (za){
-        fill(39,245,49);
+        fill(46, 218, 236);
     }else{
         fill(255);
     }
     noStroke();
     square(1200,500,150);
+
+    if (cin){
+        fill(46, 218, 236);
+    }else{
+        fill(255);
+    }
+    noStroke();
+    square(250, 700,150);
+
+    if (acc){
+        fill(46, 218, 236);
+    }else{
+        fill(255);
+    }
+    noStroke();
+    square(480,700,150);
+
+    if(go){
+        fill(46, 218, 236);
+    }else{
+        fill(255);
+    }
+    noStroke();
+    square(700,700,150);
+
+    if(le){
+        fill(46, 218, 236);
+    }else{
+        fill(255);
+    }
+    noStroke();
+    square(950,700,150);
+
+    if(mo){
+        fill(46, 218, 236);
+    }else{
+        fill(255);
+    }
+    noStroke();
+    square(1200,700,150);
     
-    if(ch) litros += 10;
-    if(pl) litros += 10;
-    if(pa) litros += 10;
-    if(ca) litros += 10;
-    if(za) litros += 10;
+    if(ch) litros += 15000;
+    if(pl) litros += 2700;
+    if(pa) litros += 10000;
+    if(ca) litros += 2200;
+    if(za) litros += 15000;
+    if(cin) litros += 2200;
+    if(acc) litros += 50;
+    if(go) litros += 800;
+    if(le) litros += 500;
+    if(mo) litros += 10000;
     textSize(40);
     fill(0);
     textFont("Times New Roman"); 
-    text("Litros de agua: " + litros, 650, 700);
+    text("Litros de agua: " + litros, 650, 1000);
     litros = 0;
     
-
-
+    
 
 
 
@@ -96,6 +148,11 @@ function draw()
     image(img3,725,520,100,100);
     image(img4,970,520,100,100);
     image(img5,1220,520,100,100);
+    image(img6,272,720,110,100);
+    image(img7,505,720,100,100);
+    image(img8,725,720,100,100);
+    image(img9,980,720,100,100);
+    image(img10,1228,720,100,100);
 
 }
 
@@ -140,6 +197,44 @@ if(mouseX >= 1200 && mouseX <= 1350 && mouseY >= 500 && mouseY <= 650){
     }
 }
 
+if(mouseX >= 250 && mouseX <= 400 && mouseY >= 700 && mouseY <= 850){
+    if(cin){
+        cin = false;
+    }else{
+        cin = true;
+    }
+}
+if(mouseX >= 480 && mouseX <= 630 && mouseY >= 700 && mouseY <= 850){
+    if(acc){
+        acc = false;
+    }else{
+        acc = true;
+    }
+}
+
+if(mouseX >= 700 && mouseX <= 850 && mouseY >= 700 && mouseY <= 850){
+    if(go){
+        go = false;
+    }else{
+        go = true;
+    }
+}
+
+if(mouseX >= 950 && mouseX <= 1100 && mouseY >= 700 && mouseY <= 850){
+    if(le){
+        le = false;
+    }else{
+        le = true;
+    }
+}
+
+if(mouseX >= 1200 && mouseX <= 1350 && mouseY >= 700 && mouseY <= 850){
+    if(mo){
+        mo = false;
+    }else{
+        mo = true;
+    }
+}
 }
 
 
@@ -150,4 +245,9 @@ function preload() {
     img3 = loadImage("/assets/pantalon.png");
     img4 = loadImage("/assets/calcetines.png");
     img5 = loadImage("/assets/zapatos.png");
+    img6 = loadImage("/assets/cinturon.png");
+    img7 = loadImage("/assets/accesorios.png");
+    img8 = loadImage("/assets/gorra.png");
+    img9 = loadImage("/assets/lentes.png");
+    img10 = loadImage("/assets/mochila.png");
 }
